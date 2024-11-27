3737b02f9f7a3132a30880a9ad006795081f0668
kiberd <kiberd22@gmail.com>
2024-11-28 00:18:53 +0900

    selector 변경

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ app.get('/price', async (req, res) => {
 
     const browser = await puppeteer.launch({
         // executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-        headless: false
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
+    // await page.setViewport({
+    //     width : 1400,              
+    //     height : 900             
+    // })
 
     const priceArry = [];
 
@@ -25,26 +30,44 @@ app.get('/price', async (req, res) => {
             nike: 1
         };
 
-        const kreamUrl = "https://kream.co.kr/search?keyword=" + model + "&tab=products";
+        const kreamUrl = "https://kream.co.kr/search?keyword=" + model;
         await page.goto(kreamUrl);
 
-        const kreamPriceSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.price.price_area > p.amount";
-        await page.waitForSelector(kreamPriceSelector);
-        const kreamPriceData = await page.$eval(
-            kreamPriceSelector, element => {
-                return element.textContent;
-            });
 
-        const kreamNameSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.product_info_area > div.title > div > p.translated_name";
-        await page.waitForSelector(kreamNameSelector);
-        const kreamNameData = await page.$eval(
-            kreamNameSelector, element => {
-                return element.textContent;
-            });
+        // const kreamPriceSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div.search_content > div.shop-content > div > div.search_result.mo > div.search_result_list > div > div > a > div.price.price_area > p.amount";
+        const kreamPriceSelector = ".amount"
+        try {
+
+            await page.waitForSelector(kreamPriceSelector, { timeout: 5000 });
+            const kreamPriceData = await page.$eval(
+                kreamPriceSelector, element => {
+                    return element.textContent;
+                });
+            price.kream = commaString2Int(kreamPriceData?.split(' ').join('').slice(0, -1));
+
+        } catch (error) {
+            console.log('Failed')
+        }
+
+
+        try {
+
+            const kreamNameSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div.search_content > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.product_info_area > div.title > div > p.translated_name";
+            await page.waitForSelector(kreamNameSelector, { timeout: 5000 });
+            const kreamNameData = await page.$eval(
+                kreamNameSelector, element => {
+                    return element.textContent;
+                });
+
+            price.name = kreamNameData;
+
+
+        } catch (error) {
+            console.log('Failed');
+        }
 
         price.model = model;
-        price.kream = commaString2Int(kreamPriceData?.split(' ').join('').slice(0, -1));
-        price.name = kreamNameData;
+
 
 
 
@@ -52,18 +75,24 @@ app.get('/price', async (req, res) => {
 
         const nikeUrl = "https://www.nike.com/kr/w?q=" + model;
         await page.goto(nikeUrl);
-        const nikeSelector = "#skip-to-products > div > div > figure > div > div.product-card__animation_wrapper > div > div > div > div";
-        await page.waitForSelector(nikeSelector);
-        const nikeData = await page.$eval(
-            nikeSelector, element => {
 
-                return element.textContent;
-            });
+        try {
+            const nikeSelector = "#skip-to-products > div > div > figure > div > div.product-card__animation_wrapper > div > div > div > div";
+            await page.waitForSelector(nikeSelector);
+            const nikeData = await page.$eval(
+                nikeSelector, element => {
 
-        price.nike = commaString2Int(nikeData?.split(' ').join('').slice(0, -1));
+                    return element.textContent;
+                });
 
+            price.nike = commaString2Int(nikeData?.split(' ').join('').slice(0, -1));
+
+        } catch (error) {
+            console.log('Failed');
+        }
+
+        console.log(price)
         priceArry.push(price);
-
 
     }
 
