e0be194df5173ae036ae78170ad560eb32211201
kiberd <kiberd22@gmail.com>
2024-11-28 19:33:44 +0900

    adjust useragent

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ app.get('/price', async (req, res) => {
     const models = ["BQ4422-161", "CZ0790-003"];
 
     const browser = await puppeteer.launch({
-        // executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
         headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
-    // await page.setViewport({
-    //     width : 1400,              
-    //     height : 900             
-    // })
+    page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
 
     const priceArry = [];
 
@@ -33,9 +29,8 @@ app.get('/price', async (req, res) => {
         const kreamUrl = "https://kream.co.kr/search?keyword=" + model;
         await page.goto(kreamUrl);
 
-
-        // const kreamPriceSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div.search_content > div.shop-content > div > div.search_result.mo > div.search_result_list > div > div > a > div.price.price_area > p.amount";
-        const kreamPriceSelector = ".amount"
+        const kreamPriceSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.price.price_area > p.amount";
+        
         try {
 
             await page.waitForSelector(kreamPriceSelector, { timeout: 5000 });
@@ -46,13 +41,12 @@ app.get('/price', async (req, res) => {
             price.kream = commaString2Int(kreamPriceData?.split(' ').join('').slice(0, -1));
 
         } catch (error) {
-            console.log('Failed')
+            console.log('Failed');
         }
-
 
         try {
 
-            const kreamNameSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div.search_content > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.product_info_area > div.title > div > p.translated_name";
+            const kreamNameSelector = "#__layout > div > div.layout__main.search-container > div.content-container > div.content > div > div.shop-content > div > div.search_result.md > div.search_result_list > div > div > a > div.product_info_area > div.title > div > p.translated_name";
             await page.waitForSelector(kreamNameSelector, { timeout: 5000 });
             const kreamNameData = await page.$eval(
                 kreamNameSelector, element => {
@@ -67,9 +61,6 @@ app.get('/price', async (req, res) => {
         }
 
         price.model = model;
-
-
-
 
         // https://www.nike.com/kr/w?q=BQ4422-161
 
@@ -96,16 +87,14 @@ app.get('/price', async (req, res) => {
 
     }
 
-
     await browser.close();
+    res.send(JSON.stringify(priceArry));
 
-    res.send(JSON.stringify(priceArry))
 });
 
 
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Crawler app listening on port ${port}`)
 });
 
 function commaString2Int(stringNumber) {
